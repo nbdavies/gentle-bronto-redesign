fc35b953a203b86b73779c30a53c33d215d25364
Nick Davies <nbdavies@gmail.com>
2017-08-14 21:51:44 -0500

    Load facebook post/events hourly via scheduled task rather than on page load

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
 	def index
-		Post.facebook_load if !Post.maximum("created_at") || (Post.maximum("created_at").to_date < Date.today)
 		@posts = Post.all
 		@upcoming = Event.where("start_time >= ?", DateTime.now).order(start_time: :asc)
 		@past = Event.where("start_time < ?", DateTime.now).order(start_time: :desc)
